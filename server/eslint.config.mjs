e8dca844fda293a33c48fbf76e3923e3a990d81d
berezenko04 <koffld@yahoo.com>
2025-08-09 16:07:32 +0300

    ✨ feat (server): added settings for prettier, eslint; added settings for read environment

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -26,9 +26,10 @@ export default tseslint.config(
   },
   {
     rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
