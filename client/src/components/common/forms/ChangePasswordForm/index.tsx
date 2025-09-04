7d47df41308b5715a7da68cb7f711738604055d1
berezenko04 <koffld@yahoo.com>
2025-09-04 16:42:26 +0300

    ✨ feat (client): added change password form

---
+++ client/src/components/common/forms/ChangePasswordForm/index.tsx
@@ -0,0 +1,104 @@
+import { Button, Stack } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+
+// components
+import PasswordInput from "../../PasswordInput";
+
+// api
+import UserService from "@/api/user/user.service";
+
+// utils
+import { catchError } from "@/utils/catchError";
+
+
+type ChangePasswordFormProps = {
+  onSuccess: () => void;
+};
+
+type ChangePasswordFormFields = {
+  currentPassword: string;
+  newPassword: string;
+  repeatNewPassword: string;
+};
+
+const ChangePasswordForm: React.FC<ChangePasswordFormProps> = ({ onSuccess }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<ChangePasswordFormFields>();
+
+  const onSubmit = async (data: ChangePasswordFormFields) => {
+    try {
+      const result = await UserService.update(data);
+      toast.success(result.message);
+      onSuccess();
+    } catch (err) {
+      catchError(err);
+    }
+  };
+
+  return (
+    <Stack component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ gap: 1.5 }}>
+      <Controller
+        name="currentPassword"
+        control={control}
+        rules={{
+          required: "Password is required",
+        }}
+        render={({ field }) => (
+          <PasswordInput
+            placeholder="Current Password"
+            value={field.value || ""}
+            setValue={field.onChange}
+            error={!!errors.currentPassword}
+            helperText={errors.currentPassword?.message}
+          />
+        )}
+      />
+
+      <Controller
+        name="newPassword"
+        control={control}
+        rules={{
+          required: "Password is required",
+          minLength: { value: 8, message: "Password is too short (minimum 8 characters)" },
+        }}
+        render={({ field }) => (
+          <PasswordInput
+            placeholder="New Password"
+            value={field.value || ""}
+            setValue={field.onChange}
+            error={!!errors.newPassword}
+            helperText={errors.newPassword?.message}
+          />
+        )}
+      />
+
+      <Controller
+        name="repeatNewPassword"
+        control={control}
+        rules={{
+          required: "Password is required",
+          validate: (value) => value === control._formValues.newPassword || "Passwords do not match",
+        }}
+        render={({ field }) => (
+          <PasswordInput
+            placeholder="New Password"
+            value={field.value || ""}
+            setValue={field.onChange}
+            error={!!errors.repeatNewPassword}
+            helperText={errors.repeatNewPassword?.message}
+          />
+        )}
+      />
+
+      <Button type="submit" variant="contained" fullWidth size="medium" disabled={isSubmitting}>
+        Change
+      </Button>
+    </Stack>
+  );
+};
+
+export default ChangePasswordForm;
